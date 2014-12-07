e293734c8dbd799ba5ae00cf70e1f1fe025eef57
miglrodri <migl.rodri@gmail.com>
2014-12-07 17:16:35 +0000

    Added if statement to call method

--- JavaProjectGit/src/com/nuvemazul/demo/HelloWorld.java
+++ JavaProjectGit/src/com/nuvemazul/demo/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("cenas");
+		
+		if (true) {
+			// call some method
+		}
 	}
 
 }
